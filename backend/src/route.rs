8b1d63492fa3227c42d70b34e4efeb154a040c74
--get <67471674+ninebolt6@users.noreply.github.com>
2023-05-21 22:43:19 +0900

    feat: テスト用の静的ページ作成

--- backend/src/route.rs
+++ backend/src/route.rs
@@ -1,3 +1,14 @@
-use actix_web::web::ServiceConfig;
+use actix_web::{
+    web::{self, ServiceConfig},
+    HttpResponse, Responder,
+};
 
-pub fn init(cfg: &mut ServiceConfig) {}
+pub fn init(cfg: &mut ServiceConfig) {
+    cfg.service(web::resource("/").route(web::get().to(index)));
+}
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(include_str!("../resources/index.html"))
+}
