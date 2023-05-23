b6682b84b16e15846c76634e4256f867836308f9
--get <67471674+ninebolt6@users.noreply.github.com>
2023-05-23 23:10:47 +0900

    feat: Broadcasterをapp_dataとしてprovideする

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,21 @@
-use actix_web::{middleware::Logger, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
+use broadcast::Broadcaster;
 use env_logger::Env;
 
+mod broadcast;
 mod route;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
 
-    HttpServer::new(|| App::new().wrap(Logger::default()).configure(route::init))
-        .bind(("0.0.0.0", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .app_data(web::Data::from(Broadcaster::new()))
+            .configure(route::init)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
