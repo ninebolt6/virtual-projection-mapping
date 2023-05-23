ef7d67abe132406b5ddcb050c381b88046b9396b
--get <67471674+ninebolt6@users.noreply.github.com>
2023-05-23 23:39:04 +0900

    feat: ログを仕込んだ

--- backend/src/broadcast.rs
+++ backend/src/broadcast.rs
@@ -6,6 +6,7 @@ use std::{
 use actix_web::rt::time::interval;
 use actix_web_lab::sse::{self, ChannelStream, Sender, Sse};
 use futures::future;
+use log::{info, trace};
 
 pub struct Broadcaster {
     inner: Mutex<BroadcasterInner>,
@@ -37,10 +38,15 @@ impl Broadcaster {
 
     pub fn start_ping(broadcaster: Arc<Self>) {
         actix_web::rt::spawn(async move {
+            info!(
+                "Starting ping on worker {}",
+                std::thread::current().name().unwrap_or("(unknown worker)")
+            );
             let mut interval = interval(Duration::from_secs(10));
             loop {
                 interval.tick().await;
                 broadcaster.remove_stale_clients().await;
+                trace!("Ping completed");
             }
         });
     }
@@ -52,17 +58,23 @@ impl Broadcaster {
             .map(|client| client.send(sse::Data::new(msg)));
 
         let _ = future::join_all(send_futures).await;
+        info!("Broadcasted message: {msg}");
     }
 
     async fn remove_stale_clients(&self) {
         let ping_futures = {
             let clients = self.inner.lock().unwrap().clients.clone();
+            trace!("Pinging {} clients", clients.len());
             clients.into_iter().map(Broadcaster::ping)
         };
 
         let result = future::join_all(ping_futures).await;
         let fresh_clients = result.into_iter().flatten().collect::<Vec<_>>();
 
+        trace!(
+            "Removed stale clients (fresh clients: {})",
+            &fresh_clients.len()
+        );
         self.inner.lock().unwrap().clients = fresh_clients;
     }
 
