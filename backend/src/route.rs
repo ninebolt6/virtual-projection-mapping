e177e23027c6f80450b74e3e154447568b66eb45
--get <67471674+ninebolt6@users.noreply.github.com>
2023-05-23 23:26:08 +0900

    feat: event sourceとなるendpointの作成

--- backend/src/route.rs
+++ backend/src/route.rs
@@ -3,12 +3,22 @@ use actix_web::{
     HttpResponse, Responder,
 };
 
+use crate::broadcast::Broadcaster;
+
 pub fn init(cfg: &mut ServiceConfig) {
-    cfg.service(web::resource("/").route(web::get().to(index)));
+    cfg.service(web::resource("/").route(web::get().to(index)))
+        .service(web::resource("/events").route(web::get().to(events)));
 }
 
 async fn index() -> impl Responder {
     HttpResponse::Ok()
         .content_type("text/html")
         .body(include_str!("../resources/index.html"))
+}
+
+async fn events(broadcaster: web::Data<Broadcaster>) -> impl Responder {
+    broadcaster
+        .register_client()
+        .await
+        .map_err(actix_web::error::ErrorNotAcceptable)
 }
